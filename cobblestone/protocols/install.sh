f18d2103bcd31788da7e025f630098d0de2f4ce5
Pittab <peter_rimmer@outlook.com>
2025-04-03 13:57:20 +0100

    update cobblestone

--- cobblestone/protocols/install.sh
+++ cobblestone/protocols/install.sh
@@ -15,6 +15,7 @@ cd ~/.agpm
 mkdir ~/.agpm/cobblestone && cd ~/.agpm/cobblestone
 curl -O https://eyescary-development.github.io/CDN/agpm_packages/cobblestone/package.zip
 unzip package.zip
+pip install lead-md
 rm package.zip
 if [ "$SHELL" == "/bin/zsh" ]; then
   export file="$HOME/.zshrc"
